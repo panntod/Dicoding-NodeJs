3ecbd69f6276a727bcd3bd47a8f935256a383020
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-01 14:05:01 +0700

    feat: added function router hapi

---
+++ src/Hapi/route.js
@@ -0,0 +1,18 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+];
+ 
+module.exports = routes;
