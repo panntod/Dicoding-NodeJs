7cf4ad904e662c0d229efc576dd5b8f31325e4bc
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-02 04:37:15 +0700

    feat: added hapi function

---
+++ src/Hapi/index.js
@@ -0,0 +1,16 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./route.js');
+ 
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+ 
+    server.route(routes);
+ 
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
